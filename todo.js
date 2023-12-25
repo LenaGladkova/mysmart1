2dcde6b56015a6f9328ec1bec562a817f3aa654a
LenaGladkova <152070490+LenaGladkova@users.noreply.github.com>
2023-12-25 14:10:30 +0200

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,27 @@
+var editObj = null;
+function butt() {
+    if(editObj !== null) {
+        editObj.closest('.txt2').innerHTML = document.getElementById("txt1").value + " <input type=\"button\" value=\"Редагувати\" onclick=\"Edit(this)\"><input type=\"button\" value=\"Х\" onclick=\"Delete(this)\">";
+        editObj = null;
+    } else {
+        let cont = document.querySelector(".p");
+        let item = document.createElement("div");
+        item.className = 'txt2';
+        item.innerHTML = document.getElementById("txt1").value;
+        item.innerHTML += " <input type=\"button\" value=\"Редагувати\" onclick=\"Edit(this)\"><input type=\"button\" value=\"Х\" onclick=\"Delete(this)\">";
+        cont.appendChild(item);
+        document.getElementById("txt1").value = '';
+        editObj = null;
+    }
+}
+
+function Delete(e) {
+    e.closest('.txt2').remove();
+}
+
+
+function Edit(e) {
+    var text2Edit = e.closest('.txt2').innerText;
+    document.getElementById('txt1').value = text2Edit;
+    editObj = e;
+}
