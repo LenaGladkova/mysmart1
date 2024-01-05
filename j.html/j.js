e612bffdbebae8f3e0d5f5443c535df1ab974f7d
LenaGladkova <152070490+LenaGladkova@users.noreply.github.com>
2024-01-05 18:31:02 +0200

    Delete j.html/j.js

--- j.html/j.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('form').addEventListener('sumbit', function(event) {
-    event.preventDefault();
-    var name = document.getElementById('nameInput').value;
-    console.log('password2: ' + name);
-})
